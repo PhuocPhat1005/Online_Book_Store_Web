e08796a8b5b1d02a43551dcac5f3318d0324b6eb
thaihuyentung04@gmail.com <thaihuyentung04@gmail.com>
2024-06-10 00:33:43 +0700

    UI SignIn & Signout withour gg

---
+++ src/frontend/online-book-store/src/components/IncorrectBox/IncorrectBox.js
@@ -0,0 +1,19 @@
+import classNames from "classnames/bind";
+import styles from './IncorrectBox.module.scss'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles)
+
+function IncorrectBox({mess="", onClose}) {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
+                <p className={cx('content')}>{mess}</p>
+                <FontAwesomeIcon className={cx('icon')} icon={faXmark} onClick={onClose}/>
+            </div>
+        </div>
+    )
+}
+
+export default IncorrectBox;
