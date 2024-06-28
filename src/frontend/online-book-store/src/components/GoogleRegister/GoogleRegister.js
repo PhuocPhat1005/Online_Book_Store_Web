d445b24471215e8dcd4a3cca44af26fd6d76ddf8
thaihuyentung04@gmail.com <thaihuyentung04@gmail.com>
2024-06-29 00:35:44 +0700

    FrontEnd - Sign in with GG#2

--- src/frontend/online-book-store/src/components/GoogleRegister/GoogleRegister.js
+++ src/frontend/online-book-store/src/components/GoogleRegister/GoogleRegister.js
@@ -1,72 +1,141 @@
-import classNames from 'classnames/bind';
+// import classNames from 'classnames/bind';
+// import { useState } from 'react';
+// import { useNavigate, useLocation } from 'react-router-dom';
 
-import styles from './GoogleRegister.module.scss';
+// import styles from './GoogleRegister.module.scss';
+// import request from '~/utils/request';
+// import Image from '~/components/Image';
+// import assets from '~/assets';
+// import { useEffect } from 'react';
+
+// const cx = classNames.bind(styles);
+
+// function GoogleRegister() {
+//     const [registerState, setRegisterState] = useState('');
+
+//     const handleRegister = async () => {
+//         try {
+//             const response = await request.get('auth/login/google');
+
+//             if (response.status === 200) {
+//                 console.log('Register successfully');
+
+//                 const googleAuthUrl = response.data.google_auth_url._headers.location;
+
+//                 googleAuthUrl
+//                     ? window.open(googleAuthUrl, '_blank')
+//                     : console.error('No URL received for Google login');
+
+//                 setRegisterState(response.data.state);
+//             } else {
+//                 console.error('Register failed');
+//             }
+//         } catch (error) {
+//             if (error.response) {
+//                 console.error('Register failed with,', error.response.data);
+//             } else if (error.request) {
+//                 console.error('No response received', error.request);
+//             } else {
+//                 console.error('Error', error.message);
+//             }
+//         }
+//     };
+
+//     useEffect(() => {
+//         console.log(registerState);
+//         const handleRegisterCallback = async () => {
+//             try {
+//                 const response = await request.get('auth/google-callback', {
+//                     params: { state: registerState },
+//                 });
+
+//                 if (response.status === 200) {
+//                     console.log('(callback) Register successfully');
+//                     console.log(response.data);
+//                 } else {
+//                     console.error('(callback) Register failed');
+//                 }
+//             } catch (error) {
+//                 if (error.response) {
+//                     console.error('(callback) Register failed with,', error.response.data);
+//                 } else if (error.request) {
+//                     console.error('(callback) No response received', error.request);
+//                 } else {
+//                     console.error('(callback) Error', error.message);
+//                 }
+//             }
+//         };
+//         handleRegisterCallback();
+//     }, [registerState]);
+
+//     return (
+//         <div className={cx('gg-signup')}>
+//             <Image className={cx('gg-logo')} src={assets.google_logo} />
+//             <p className={cx('signup-text')} onClick={handleRegister}>
+//                 Sign Up with Google
+//             </p>
+//         </div>
+//     );
+// }
+
+// export default GoogleRegister;import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import request from '~/utils/request';
-import Image from '~/components/Image';
-import assets from '~/assets';
 import { useEffect } from 'react';
 
-const cx = classNames.bind(styles);
-
 function GoogleRegister() {
+    const location = useLocation();
+
     const handleRegister = async () => {
         try {
             const response = await request.get('auth/login/google');
-
             if (response.status === 200) {
-                console.log('Register successfully');
-                const googleAuthUrl = response.data.google_auth_url._headers.location;
+                const googleAuthUrl = response.data.google_auth_url_headers.location;
 
-                googleAuthUrl
-                    ? window.open(googleAuthUrl, '_blank')
-                    : console.error('No URL received for Google login');
+                // Chuyển hướng người dùng đến URL xác thực của Google
+                window.location.href = googleAuthUrl;
             } else {
                 console.error('Register failed');
             }
         } catch (error) {
             if (error.response) {
-                console.error('Register failed with,', error.response.data);
+                console.error('(callback) Register failed with,', error.response.data);
             } else if (error.request) {
-                console.error('No response received', error.request);
+                console.error('(callback) No response received', error.request);
             } else {
-                console.error('Error', error.message);
+                console.error('(callback) Error', error.message);
             }
         }
     };
 
     useEffect(() => {
-        const handleRegisterCallback = async () => {
-            try {
-                const response = await request.get('auth/google-callback');
+        const urlParams = new URLSearchParams(location.search);
+        const state = urlParams.get('state');
 
-                const { access_token, refresh_token } = response.data;
-                console.log(access_token, refresh_token);
+        if (state) {
+            const handleRegisterCallback = async () => {
+                try {
+                    const response = await request.get('auth/google-callback', {
+                        params: { state },
+                    });
 
-                if (response.status === 200) {
-                    console.log('(callback) Register successfully');
-                    console.log(response.data);
-                } else {
-                    console.error('(callback) Register failed');
+                    if (response.status === 200) {
+                        console.log('Callback successful');
+                        console.log(response.data);
+                    } else {
+                        console.error('Callback failed');
+                    }
+                } catch (error) {
+                    console.error('Callback error', error.message);
                 }
-            } catch (error) {
-                if (error.response) {
-                    console.error('(callback) Register failed with,', error.response.data);
-                } else if (error.request) {
-                    console.error('(callback) No response received', error.request);
-                } else {
-                    console.error('(callback) Error', error.message);
-                }
-            }
-        };
-        handleRegisterCallback();
-    }, []);
+            };
+            handleRegisterCallback();
+        }
+    }, [location.search]);
 
     return (
-        <div className={cx('gg-signup')}>
-            <Image className={cx('gg-logo')} src={assets.google_logo} />
-            <p className={cx('signup-text')} onClick={handleRegister}>
-                Sign Up with Google
-            </p>
+        <div>
+            <button onClick={handleRegister}>Sign Up with Google</button>
         </div>
     );
 }
