668da205f89e422b8b6ba2e9c580f4b81123b09c
thanhto24 <tqthanh22@clc.fitus.edu.vn>
2024-09-24 14:23:30 +0700

    Change baseURL

--- src/frontend/admin-online-book-store/src/utils/request.js
+++ src/frontend/admin-online-book-store/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: 'https://online-book-store-web.onrender.com/',
     headers: {
         // 'Content-Type': 'application/json',
         // 'Content-Type': 'application/x-www-form-urlencoded',
